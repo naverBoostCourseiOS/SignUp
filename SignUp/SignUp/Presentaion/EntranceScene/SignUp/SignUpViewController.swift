1373bc255da031ab904bb328ed78d024bf51b5cd
kangddong <rkdehddud96@naver.com>
2023-02-19 01:44:11 +0900

    page 1 done

---
+++ SignUp/SignUp/Presentaion/EntranceScene/SignUp/SignUpViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SignUpViewController.swift
+//  SignUp
+//
+//  Created by 강동영 on 2023/02/19.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    static let identifier: String = String(describing: SignUpViewController.self)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
